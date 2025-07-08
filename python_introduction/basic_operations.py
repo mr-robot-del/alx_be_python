612cef0af8e6f36aac99a9b47fb81488442a50a0
Dev-Godwin <davidgodwin065@gmail.com>
2025-07-08 10:40:59 -0700

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+mltp = number1 * number2
+
+print(f"Additiion of {number1} and {number2} is {add}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {mltp}")
