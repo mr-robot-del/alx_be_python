97e2db87a12130decc62cfed288254e5aa08d619
mr-robot-del <fenzyaboy4life@gmail.com>
2025-07-25 13:36:21 -0700

    pattern_drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+size = int(input("Enter the size of the pattern: "))
+
+while size <= 0:
+    print("Please enter a positive integer.")
+    size = int(input("Enter the size of the pattern: "))
+
+row = 1
+
+while row <= size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
+
